51be61e2c74bcd8eb7f1536dfdbb773e27b55b37
zhoujiang <zhoujiang668@gmail.com>
2021-12-02 19:32:57 +0800

    Update bert_for_ner.py

--- models/bert_for_ner.py
+++ models/bert_for_ner.py
@@ -52,20 +52,19 @@ class BertCrfForNer(BertPreTrainedModel):
         self.crf = CRF(num_tags=config.num_labels, batch_first=True)
         self.init_weights()
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None,labels=None):
-        outputs =self.bert(input_ids = input_ids,attention_mask=attention_mask,token_type_ids=token_type_ids)
+#     def forward(self, input_ids, attention_mask=None, token_type_ids=None, labels=None):
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        outputs =self.bert(input_ids=input_ids,attention_mask=attention_mask,token_type_ids=token_type_ids)
         sequence_output = outputs[0]
         sequence_output = self.dropout(sequence_output)
         logits = self.classifier(sequence_output)
         outputs = (logits,)
-        if labels is not None:
-            # ctf.decode应该作为前向，目前的foward是在计算loss
-            pred = self.crf(logits, attention_mask)
-            outputs = (pred,) + outputs
-        return outputs 
-    def loss_fn(self, emissions, tags, mask):
-        return -1 * self.crf.loss_fn(emissions = logits, tags=inputs['labels'], mask=inputs['attention_mask'])
+        pred = self.crf(logits, attention_mask,1,0)
+        outputs = (pred,) + outputs
+        return outputs
 
+    def loss_fn(self, emissions, tags, mask):
+        return -1 * self.crf.loss_fn(emissions = emissions, tags = tags, mask = mask, reduction='mean')
 
 class BertSpanForNer(BertPreTrainedModel):
     def __init__(self, config,):
