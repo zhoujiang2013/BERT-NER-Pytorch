bec04102beb97a6c41d2a5315cb6b16d60d99592
zhoujiang <zhoujiang668@gmail.com>
2021-12-07 15:34:24 +0800

    Update model_config.py

--- conf/model_config.py
+++ conf/model_config.py
@@ -4,5 +4,5 @@ class BilstmCrfConfig():
         embedding_size = config.embedding_size
         vocab_size = config.vocab_size
         hidden_size = config.hidden_size
-        hidden_dropout_prob = config.hidden_dropout_prob
+        dropout_prob = config.dropout_prob
         num_labels = config.num_labels
