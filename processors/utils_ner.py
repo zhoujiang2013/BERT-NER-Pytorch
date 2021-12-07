c8dc31f944a5c0176c749201c4fcc1f36cdc1d61
zhoujiang <zhoujiang668@gmail.com>
2021-12-07 14:59:17 +0800

    Update utils_ner.py

--- processors/utils_ner.py
+++ processors/utils_ner.py
@@ -31,7 +31,7 @@ class DataProcessor(object):
     @classmethod
     def _read_text(self,input_file):
         lines = []
-        with open(input_file,'r') as f:
+        with open(input_file,'r', encoding='utf-8-sig') as f:
             words = []
             labels = []
             for line in f:
@@ -55,7 +55,7 @@ class DataProcessor(object):
     @classmethod
     def _read_json(self,input_file):
         lines = []
-        with open(input_file,'r') as f:
+        with open(input_file,'r', encoding='utf-8-sig') as f:
             for line in f:
                 line = json.loads(line.strip())
                 text = line['text']
