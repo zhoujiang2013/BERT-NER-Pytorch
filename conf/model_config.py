b89bc78687701c99c7bde9a89a6225e6b4e90b09
zhoujiang <zhoujiang668@gmail.com>
2021-12-07 15:49:29 +0800

    Update model_config.py

--- conf/model_config.py
+++ conf/model_config.py
@@ -1,8 +1,8 @@
 
 class BilstmCrfConfig():
     def __init__(self, config):
-        embedding_size = config.embedding_size
-        vocab_size = config.vocab_size
-        hidden_size = config.hidden_size
-        dropout_prob = config.dropout_prob
-        num_labels = config.num_labels
+        self.embedding_size = config.embedding_size
+        self.vocab_size = config.vocab_size
+        self.hidden_size = config.hidden_size
+        self.dropout_prob = config.dropout_prob
+        self.num_labels = config.num_labels
