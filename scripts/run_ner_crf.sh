042e9db3f6721d545ebc3f07a4e0968a65289ff8
zhoujiang <zhoujiang668@gmail.com>
2021-12-07 15:13:59 +0800

    Update run_ner_crf.sh

--- scripts/run_ner_crf.sh
+++ scripts/run_ner_crf.sh
@@ -14,8 +14,8 @@ python run_ner_crf.py \
   --data_dir=$DATA_DIR/${TASK_NAME}/ \
   --train_max_seq_length=128 \
   --eval_max_seq_length=512 \
-  --per_gpu_train_batch_size=24 \
-  --per_gpu_eval_batch_size=24 \
+  --per_train_batch_size=24 \
+  --per_eval_batch_size=24 \
   --learning_rate=3e-5 \
   --crf_learning_rate=1e-3 \
   --num_train_epochs=4.0 \
